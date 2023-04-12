ad6e0952f18a38ea3b11c101855aea9b02da38d2
Steven Oyer <contact.stevenoyer@gmail.com>
2023-04-12 15:34:01 +0200

    Récupération/Sauvegarde des catégories pour Hikashop

---
+++ plugins/hikashop/hikabarr/src/Hikashop/Class/Category.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace Systrio\Plugins\Hikabarr\Hikashop\Class;
+
+use hikashopCategoryClass;
+use Joomla\CMS\Factory;
+use Systrio\Plugins\Hikabarr\Hikashop\Models\CategoryModel;
+
+class Category
+{
+    private CategoryModel $category;
+    
+    public function __construct(CategoryModel $category)
+    {
+        $this->category = $category;
+    }
+
+    public function save(): mixed
+    {
+        if (!empty($this->get()))
+        {
+            $this->category->category_id = $this->get()->category_id;
+        }
+
+        $hikashopCategory = new hikashopCategoryClass;
+        return $hikashopCategory->save($this->category);
+    }
+
+    public function delete(): bool|null
+    {
+        $hikashopCategory = new hikashopCategoryClass;
+        return $hikashopCategory->delete($this->get()->category_id);
+    }
+
+    public function get(): mixed
+    {
+        if (is_null($this->category->category_id))
+        {
+            return $this->getByName();
+        }
+        
+        return $this->getById();
+    }
+
+    public function getById(): mixed
+    {
+        $hikashopCategory = new hikashopCategoryClass;
+        return $hikashopCategory->get($this->category->category_id);
+    }
+
+    public function getByName(): mixed
+    {
+        $db = Factory::getContainer()->get('DatabaseDriver');
+
+        $query = $db
+            ->getQuery(true)
+            ->select('category_id')
+            ->from('#__hikashop_category')
+            ->where($db->qn('category_name') . ' = ' . $db->q($this->category->category_name));
+
+        if (!empty($db->setQuery($query)->loadObject()->category_id))
+        {
+            $this->category->category_id = $db->setQuery($query)->loadObject()->category_id;
+            return $this->getById();
+        }
+
+        return false;
+    }
+
+}
