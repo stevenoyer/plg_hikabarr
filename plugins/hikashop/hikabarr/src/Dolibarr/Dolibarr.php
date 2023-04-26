5bb857a66346ad7972d8a0c903a9656d49186fb7
Steven Oyer <contact.stevenoyer@gmail.com>
2023-04-26 15:44:05 +0200

    Suppression de code inutile

--- plugins/hikashop/hikabarr/src/Dolibarr/Dolibarr.php
+++ plugins/hikashop/hikabarr/src/Dolibarr/Dolibarr.php
@@ -7,14 +7,10 @@ use Systrio\Plugins\Hikabarr\Dolibarr\RequestApi;
 class Dolibarr
 {
     public RequestApi $requestApi;
-    private string $api_url;
-    private string $token;
 
     public function __construct(string $api_url, string $token)
     {
         $this->requestApi = new RequestApi($api_url, $token);
-        $this->api_url = $api_url;
-        $this->token = $token;
     }
 
     public function requestApi()
