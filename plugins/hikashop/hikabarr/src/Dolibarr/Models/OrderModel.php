e2c89ed6e8360ad318be6f374a42e1f88830ff21
Steven Oyer <contact.stevenoyer@gmail.com>
2023-04-26 15:43:42 +0200

    Modèle de commande

--- plugins/hikashop/hikabarr/src/Dolibarr/Models/OrderModel.php
+++ plugins/hikashop/hikabarr/src/Dolibarr/Models/OrderModel.php
@@ -5,5 +5,17 @@ namespace Systrio\Plugins\Hikabarr\Dolibarr\Models;
 class OrderModel
 {
 
+    public ?int $id = null;
+    public ?int $socid; // Id ThirdParty
+    public ?int $date;
+    public ?array $lines; // array id product and quantity
+    public ?int $idwarehouse = null;
+    public ?int $type;
+    public ?string $ref_ext; // We use Order Number on Hikashop side
+
+    public function __construct()
+    {
+        $this->type = 0;
+    }
 
 }
