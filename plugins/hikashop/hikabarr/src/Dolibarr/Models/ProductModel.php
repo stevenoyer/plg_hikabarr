f79a7977a7445ff52b5839086214bc2c274ce87f
Steven Oyer <contact.stevenoyer@gmail.com>
2023-04-26 15:43:33 +0200

    Modèle de produit

--- plugins/hikashop/hikabarr/src/Dolibarr/Models/ProductModel.php
+++ plugins/hikashop/hikabarr/src/Dolibarr/Models/ProductModel.php
@@ -5,5 +5,10 @@ namespace Systrio\Plugins\Hikabarr\Dolibarr\Models;
 class ProductModel
 {
 
+    public ?int $id;
+    public ?string $ref;
+    public ?string $label;
+    public ?float $subprice;
+    public ?string $desc;
 
 }
